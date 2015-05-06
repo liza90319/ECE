9de767f3a199ebb94d6113f7f533b23cb9fc3856
Li, Biqing <Lisa@lawn-128-61-51-214.lawn.gatech.edu>
2015-05-06 16:35:55 -0400

    Added some scripts

--- js/script.js
+++ js/script.js
@@ -1,46 +1,97 @@
 // JavaScript Document
 
-	var apiKey = '45228092';
-	var apiSecret = 'a678c7fa4650bb08c4aaf9efcbaf830afb84388f';
-	var OpenTok = require('opentok'),
-    opentok = new OpenTok(apiKey, apiSecret);
-	
-	// Create a session that will attempt to transmit streams directly between
-	// clients. If clients cannot connect, the session uses the OpenTok TURN server:
-	opentok.createSession(function(err, session) {
-	  if (err) return console.log(err);
-	  // save the sessionId
-	  db.save('session', session.sessionId, done);
-	});
-	
-	// The session will the OpenTok Media Router:
-	opentok.createSession({mediaMode:"routed"}, function(err, session) {
-	  if (err) return console.log(err);
-	  // save the sessionId
-	  db.save('session', session.sessionId, done);
-	});
-	
-	// A Session with a location hint
-	opentok.createSession({location:'12.34.56.78'}, function(err, session) {
-	  if (err) return console.log(err);
-	  // save the sessionId
-	  db.save('session', session.sessionId, done);
-	});
-	
-	
-	// Generate a Token from just a sessionId (fetched from a database)
-	token = opentok.generateToken(sessionId);
-	
-	// Genrate a Token from a session object (returned from createSession)
-	token = session.generateToken();
-	
-	// Set some options in a Token
-	token = session.generateToken({
-	  role :       'moderator',
-	  expireTime : (new Date().getTime() / 1000)+(7 * 24 * 60 * 60), // in one week
-	  data :       'name=Johnny'
-	});
-	
-	
+		var apiKey    = "45228092";
+		var sessionId = "1_MX40NTIyODA5Mn5-MTQzMDk0MTY1NzE2NH5QcTRydkZyU1Iya21taFhDVmN2UDVZc3N-fg";
+		var token     = "T1==cGFydG5lcl9pZD00NTIyODA5MiZzaWc9MmExMTY1YTNhMTdlNzQwMGJiNjk4NjY1OGU1NDVkYWY5NmJhYzcxMDpyb2xlPXB1Ymxpc2hlciZzZXNzaW9uX2lkPTFfTVg0ME5USXlPREE1TW41LU1UUXpNRGswTVRZMU56RTJOSDVRY1RSeWRrWnlVMUl5YTIxdGFGaERWbU4yVURWWmMzTi1mZyZjcmVhdGVfdGltZT0xNDMwOTQxNjY0Jm5vbmNlPTAuNDg4NTkyNjUxMDI1MjU3NzMmZXhwaXJlX3RpbWU9MTQzMTU0NjQ1MCZjb25uZWN0aW9uX2RhdGE9";
+		 
+		var session;
+		var connectionCount = 0;
+		
+		
+		 
+		function connect() {
+		  if (OT.checkSystemRequirements() == 0) {
+			console.log("The client does not support WebRTC.");
+		  } else {
+			// Replace apiKey and sessionId with your own values:
+			session = OT.initSession(apiKey, sessionId);
+			session.on({
+			  connectionCreated: function (event) {
+				connectionCount++;
+				console.log(connectionCount + " connections.");
+			  },
+			  connectionDestroyed: function (event) {
+				connectionCount--;
+				console.log(connectionCount + " connections.");
+			  },
+			   sessionDisconnected: function sessionDisconnectHandler(event) {
+				// The event is defined by the SessionDisconnectEvent class
+				console.log("Disconnected from the session.");
+				//document.getElementById('disconnectBtn').style.display = 'none';
+				if (event.reason == "networkDisconnected") {
+				  alert("Your network connection terminated.")
+				}
+			  }
+			});
+			// Replace token with your own value:
+			session.connect(token, function(error) {
+			  if (error) {
+				OT.log("Unable to connect: ", error.message);
+			  } else {
+				//document.getElementById('disconnectBtn').style.display = 'block';
+				console.log("Connected to the session.");
+				connectionCount = 1;
+			  }
+			});
+		  }
+		}
+		 
+		 
+		 
+		function disconnect() {
+		  session.disconnect();
+		}
+		 
+		connect();
+//Publishing Stream
 
-	
+	var session;
+	var publisher;
+	 
+	if (OT.checkSystemRequirements() == 1) {
+		// Replace with the replacement element ID:
+		publisher = OT.initPublisher('myPublisher');
+		publisher.on({
+			streamCreated: function (event) {
+				console.log("Publisher started streaming.");
+			},
+			streamDestroyed: function (event) {
+				console.log("Publisher stopped streaming. Reason: "
+				   + event.reason);
+			}
+		});
+	 
+		// Replace apiKey and sessionID with your own values:
+		session = OT.initSession(apiKey, sessionId);
+		// Replace token with your own value:
+		session.connect(token, function (error) {
+			if (session.capabilities.publish == 1) {
+				session.publish(publisher);
+			} else {
+				console.log("You cannot publish an audio-video stream.");
+			}
+		});
+	} else {
+		console.log("The client does not support WebRTC.");
+	}
+
+
+//Subscribing to a stream
+	session.on("streamCreated", function (event) {
+	   console.log("New stream in the session: " + event.stream.streamId);
+	});
+	// Replace with a valid token:
+	session.connect(token);
+	session.subscribe(stream, 'left_camera');
+
+	  
